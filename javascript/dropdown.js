1668f0b318e62df15a7d9dea079f94becc8aa374
ilgiz <ilgizayupov7@gmail.com>
2020-02-20 18:07:12 +0500

    fix bug

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -1,19 +1,40 @@
-const dropdown = document.querySelector('.dropdown');
-const dropdownChild = document.querySelector('.dropdown-child');
-const menuBtn = document.querySelector('.mainmenubtn')
+class Dropdown {
+    constructor(obj) {
+        this.nameDropdown = document.querySelector(obj.nameDropdown)
+        this.dropdownChild = document.querySelector(obj.dropdownChild)
+        this.dropdownBtn = document.querySelector(obj.dropdownBtn)
 
-dropdown.addEventListener('click', function () {
-    dropdownChild.style.display == 'block' ? dropdownChild.style.display = 'none' : dropdownChild.style.display = 'block';
-    menuBtn.innerHTML = `${'&#9776;'}`;
-    menuBtn.style.color = `black`
-    if(dropdownChild.style.display == 'block'){
-        dropdownChild.style = `
-        display:block;
-        position: absolute;
-        top: ${menuBtn.clientHeight + 5}px;
-        right: 0;
-        `
-        menuBtn.innerHTML = `${'&#10006'}`;
-        menuBtn.style.color = 'red'
+
+        this.nameDropdown.addEventListener('click', () => { this.click() })
     }
+    click() {
+        this.dropdownChild.style.display == 'block' ? this.dropdownChild.style.display = 'none' : this.dropdownChild.style.display = 'block';
+
+        this.check()
+
+        this.dropdownBtn.innerHTML = `${'&#9776;'}`
+        this.dropdownBtn.style.color = `black`
+    }
+    check() {
+        if (this.dropdownChild.style.display == 'block') {
+            this.dropdownChild.style = `
+            display:block;
+            position: absolute;
+            width: 150px;
+            height:max-content;
+            padding:10px;
+            top: ${this.dropdownBtn.clientHeight + 5}px;
+            right: 0;
+            `
+            this.dropdownBtn.innerHTML = `${'&#10006'}`;
+            this.dropdownBtn.style.color = 'red'
+        }
+    }
+}
+
+let dropdown = new Dropdown({
+    nameDropdown: ' .header__dropdown ',
+    dropdownChild: '.dropdown-child',
+    dropdownBtn: '.header__dropdown-btn'
+
 })
